f8beac4e8902d8b5d4ba9f04945615891a6deefe
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-09-08 15:39:01 -0700

    Create Puzzle6.js

---
+++ Puzzle6.js
@@ -0,0 +1,28 @@
+//Use red dot to collect green dots
+
+//Control red dot with following commands
+
+|Right
+
+[Recipe](1);
+If getcolor Salmon
+|Up
+|Up
+|Up
+else
+|Down
+|Down
+|Down
+|Left
+|Down
+if getcolor Salmon
+|Left
+|Up
+if getcolor blue 
+|Right
+|Up
+
+//Interact the red dot's enviorment using the following commands:
+//remainingdots();
+//getcolor();
+//setcolor();
