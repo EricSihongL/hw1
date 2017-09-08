8a71b5faca80427c895f49cd719639e71ea488e9
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-09-08 15:24:23 -0700

    Create puzzle3.js

---
+++ puzzle3.js
@@ -0,0 +1,21 @@
+//Use red dot to collect green dots
+
+//Control red dot with following commands
+
+Down();
+Down();
+Get Color();
+Save Color();
+Down(); 
+Down();
+Down();
+Right();
+Right();
+Right();
+Right();
+Up();
+
+//Interact the red dot's enviorment using the following commands:
+//remainingdots();
+//getcolor();
+//setcolor();
