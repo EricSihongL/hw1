8f0dc95147146fcf5559586671085c1eed36dad3
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-09-08 15:34:05 -0700

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,22 @@
+//Use red dot to collect green dots
+
+//Control red dot with following commands
+
+|Right
+|Right
+
+[Recipe](1);
+Repeat if getcolor Blue
+|Down 
+|Right
+|Right
+else
+|up
+|Right
+|Right
+
+|Right
+//Interact the red dot's enviorment using the following commands:
+//remainingdots();
+//getcolor();
+//setcolor();
