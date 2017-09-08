9f169116023bf07dc35174f1302df7191417ea91
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-09-08 15:28:20 -0700

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,21 @@
+//Use red dot to collect green dots
+
+//Control red dot with following commands
+
+Right ();
+
+[Recipe]();
+Repeat Down if Red Dot doesn’t get color();
+[Directions]();
+*Save Color();
+
+*Down();
+*Right();
+*Right();
+Up();
+Right();
+
+//Interact the red dot's enviorment using the following commands:
+//remainingdots();
+//getcolor();
+//setcolor();
